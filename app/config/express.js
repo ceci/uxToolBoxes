03d33f3dcce4a3072c47b9c99be46ff03b06befb
Marco Bruno <marco.bruno.br@gmail.com>
2016-09-15 21:25:39 -0300

    Configurado a rota dos arquivos publicos e criado um reset.css basico

--- app/config/express.js
+++ app/config/express.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 
 module.exports = function () {
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+
+    app.use(express.static('./app/public'));
 
     return app;
 }
